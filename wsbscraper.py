52261760210d0844f2078ecad8c9dff9b2c62516
amansarosh <amansarosh@gmail.com>
2021-08-27 18:40:30 -0700

    Added user prompts as well as loading indicator for different subs

--- wsbscraper.py
+++ wsbscraper.py
@@ -1,4 +1,7 @@
+# to-do: read from config file as well as credentials
+
 import praw
+import time
 
 # import client details
 reddit = praw.Reddit(client_id='xxx',
@@ -7,15 +10,24 @@ reddit = praw.Reddit(client_id='xxx',
                      password='xxx',
                      user_agent='xxx')
 
-# scraping destination
+# Questions
+sub_answer = str(input('What Subreddit would you like to see? '))
+listing_num = int(input(f'How many posts of {sub_answer} would you like to see? (Pick from 1-1000) '))
 
-# to-do: read from config file as well as credentials
-subreddit = reddit.subreddit('python')
+print('0% ####')
+time.sleep(.5)
+print('25% ##################')
+time.sleep(.5)
+print('50% ########################')
+time.sleep(.5)
+print('75% ##############################')
+time.sleep(.5)
+print('100% ####################################')
 
-# posts to retrieve
-hot_wsb = subreddit.hot(limit=5)
+# variable definitions
+subreddit = reddit.subreddit(sub_answer)
+hot_wsb = subreddit.hot(limit=listing_num)
 
 # loop over post and see post metrics
 for submission in hot_wsb:
-    if not submission.stickied:
-        print('Title: {} // Upvotes: {}'.format(submission.title, submission.ups,))
+    print('Title: {} // Upvotes: {}'.format(submission.title, submission.ups,))
