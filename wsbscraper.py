8db92bc28c15b4494e950059bd29e5d17a3eab98
amansarosh <amansarosh@gmail.com>
2021-09-04 00:09:48 -0700

    Comment logic added, working out some bugs. Notes at end.

--- wsbscraper.py
+++ wsbscraper.py
@@ -1,5 +1,3 @@
-# to-do: read from config file as well as credentials
-
 import praw
 import time
 
@@ -10,19 +8,32 @@ reddit = praw.Reddit(client_id='xxx',
                      password='xxx',
                      user_agent='xxx')
 
-# Questions
-sub_answer = str(input('What Subreddit would you like to see? '))
-listing_num = int(input(f'How many posts of {sub_answer} would you like to see? (Pick from 1-1000) '))
+# <-----------------Questions--------------------------------------------> #
+sub_answer = input('What Subreddit would you like to see: ')  # Left as input as subs are ints as well
 
-print('0% ####')
-time.sleep(.5)
-print('25% ##################')
-time.sleep(.5)
-print('50% ########################')
-time.sleep(.5)
-print('75% ##############################')
-time.sleep(.5)
-print('100% ####################################')
+# Title Amt
+while True:
+    try:
+        listing_num = int(input(f'How many posts of {sub_answer} would you like to see? (Pick from 1-1000): '))
+        if listing_num > 0:
+            break
+        print("Invalid number entered")
+    except Exception as e:
+        print(e)
+
+# <-----------------Questions--------------------------------------------> #
+
+# print('0% ####')
+# time.sleep(.5)
+# print('25% ##################')
+# time.sleep(.5)
+# print('50% ########################')
+# time.sleep(.5)
+# print('75% ##############################')
+# time.sleep(.5)
+# print('100% ####################################')
+
+# <-----------------Questions--------------------------------------------> #
 
 # variable definitions
 subreddit = reddit.subreddit(sub_answer)
@@ -30,4 +41,31 @@ hot_wsb = subreddit.hot(limit=listing_num)
 
 # loop over post and see post metrics
 for submission in hot_wsb:
-    print('Title: {} // Upvotes: {}'.format(submission.title, submission.ups,))
+    print('Title: {} // Upvotes: {}'.format(submission.title, submission.ups, ))
+
+# comment function
+comment_limit = int(input('How many comments would you like to see would you like to see: '))
+
+submission.comments.replace_more(comment_limit)  # define out comments
+
+for comment in submission.comments.list():  # looping over comments
+    print(comment.body)
+    if len(comment.replies) > 0:
+        for reply in comment.replies:
+            print('||||||', reply.body)
+
+#
+
+while True:
+    try:
+        print('===')
+        mention_questions = str(input('Would you like to count mentions?: '))
+        if mention_questions == 'yes':
+            mentions_ans = (input('What mentions?: '))
+            print(f'You said {mentions_ans}')
+            break
+    except Exception as e:
+        print(e)
+
+
+# Code was basically dumped in, will organize and fix bugs later this week, hopefully.
